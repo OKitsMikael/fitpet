482c88b523d90e09327e840aca7cb142a685019b
Mikael Davis <mikaelgdavis@gmail.com>
2017-01-22 18:13:40 -0500

    add layout file

--- server.rb
+++ server.rb
@@ -1,7 +1,7 @@
-require_relative './settings'
+require_relative './settings.rb'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/' do
- erb :splash
+ erb :splash, layout: :layout
 end
